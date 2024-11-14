8f8b3778c7fc511e9a8eb5e5b84233e0db68ef0e
ADODD300 <ADODD300@caledonian.ac.uk>
2024-11-14 18:03:07 +0000

    restoring original code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,44 +1,24 @@
-import java.util.logging.*;
-class Dec2Hex {
-    private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
-    public static int Arg1;
+import java.util.Scanner;
 
-    public static void main(String args[]) {
-        LogManager.getLogManager().reset();
-        logger.setLevel(Level.INFO);
+class Dec2Hex
+{
+public static int Arg1;
+    public static void main(String args[])    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        if (args.length == 0) {
-            logger.severe("Error: No input argument provided.");
-            return;
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
         }
 
-        try {
-            Arg1 = Integer.parseInt(args[0]);
-            int num = Arg1;
-            String hexadecimal = "";
-            logger.info(String.format("Starting conversion for decimal value: %d", num));
-            hexadecimal = toHexadecimal(num);
-            logger.info(String.format("Hexadecimal representation for %d is: %s", num, hexadecimal));
-            logger.info("Conversion process completed.");
-        } catch (NumberFormatException e) {
-            logger.severe("Error: Invalid input. Please provide a valid integer.");
-        }
-    }
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-    public static String toHexadecimal(int num) {
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        String hexadecimal = "";
-
-        if (num == 0) {
-            return "0";
-        }
-
-        while (num != 0) {
-            int rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
-        }
-
-        return hexadecimal;
     }
 }
