6b2f191876a21a0aabec5e83aaa108f53163c0ac
ADODD300 <ADODD300@caledonian.ac.uk>
2024-11-14 14:13:56 +0000

    changing dec2hex again

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,7 +3,6 @@ class Dec2Hex {
 
     public static void main(String args[]) {
         Arg1 = Integer.parseInt(args[0]);
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num;
         num = Arg1;
         String hexadecimal = "";
@@ -14,6 +13,7 @@ class Dec2Hex {
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
     }
+
     public static String toHexadecimal(int num) {
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         String hexadecimal = "";
@@ -28,4 +28,3 @@ class Dec2Hex {
     }
 
 }
-
