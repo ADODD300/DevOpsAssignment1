34b0938657bd233fdd054cda371d0bd46767c7fc
ADODD300 <ADODD300@caledonian.ac.uk>
2024-11-14 17:15:01 +0000

    fixing Dec2Hex errors

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,16 +2,24 @@ class Dec2Hex {
     public static int Arg1;
 
     public static void main(String args[]) {
-        Arg1 = Integer.parseInt(args[0]);
-        int rem, num;
-        num = Arg1;
-        String hexadecimal = "";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        if (args.length == 0) {
+            System.out.println("Error: No input argument provided.");
+            return; // Exits the program if no argument is given.
+        }
 
-        hexadecimal = toHexadecimal(num);
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+            int rem, num;
+            num = Arg1;
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+            hexadecimal = toHexadecimal(num);
 
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid input. Please provide a valid integer.");
+        }
     }
 
     public static String toHexadecimal(int num) {
@@ -26,5 +34,4 @@ class Dec2Hex {
 
         return hexadecimal;
     }
-
 }
